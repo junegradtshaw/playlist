a776e32df3c6502b67917654a43180ee91068fe9
June Shaw <june.shaw@me.com>
2015-12-09 15:50:23 -0700

    initial commit

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+//
+// If user clicks choose tracks, open playlist page.
+$('#chooseTracks').click(function () {
+  // window.open("www.youraddress.com","_self")
+  window.open("file:///Users/June/Workspace/playlist/Public/tracks.html");
+});
